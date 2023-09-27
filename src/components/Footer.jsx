c2b5a6fc2096ac2035c4f6ec7476a349b7154511
박현성 <@172.16.11.203>
2023-09-27 18:54:51 +0900

    미디어쿼리 적용 커밋 --

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,32 +1,7 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 
 export default function Footer() {
-  const scrollToTop = () => {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    })
-  }
-
-  const [showButton, setShowButton] = useState(false);
-
-  useEffect(() => {
-      const handleShowButton = () => {
-          if (window.scrollY > 500) {
-              setShowButton(true)
-          } else {
-              setShowButton(false)
-          }
-      }
-
-      console.log(window.scrollY)
-      window.addEventListener("scroll", handleShowButton)
-      return () => {
-          window.removeEventListener("scroll", handleShowButton)
-      }
-  }, [])
-
-  return showButton && (
+  return (
     <footer id='footer' className='footer-container'>
       <div className="global-marginwrap">
         <div className="footer-wrapper">
@@ -34,13 +9,6 @@ export default function Footer() {
           <p className='footerTxt'>© 2023 SUNGHUN. All rights reserved.</p>
         </div>
       </div>
-      <button id='scroll-btn' onClick={scrollToTop}>
-        <svg width="100%" height="100%" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <g clipPath="url(#clip0_533_298)">
-            <path d="M13.3686 27.6926C13.3685 28.5939 14.0992 29.3245 15.0004 29.3245C15.9016 29.3245 16.6322 28.5939 16.6322 27.6926L13.3686 27.6926ZM16.1543 1.15349C15.517 0.51622 14.4838 0.516203 13.8465 1.15349L3.46163 11.5384C2.82436 12.1756 2.82436 13.2089 3.46163 13.8461C4.0989 14.4834 5.13211 14.4834 5.76938 13.8461L15.0004 4.61512L24.2314 13.8461C24.8687 14.4834 25.9019 14.4834 26.5391 13.8461C27.1764 13.2088 27.1764 12.1757 26.5391 11.5384L16.1543 1.15349ZM16.6322 27.6926L16.6322 2.30737L13.3686 2.30736L13.3686 27.6926L16.6322 27.6926Z" fill="currentcolor"></path>
-          </g>
-        </svg>
-      </button>
     </footer>
   );
 }
--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -13,18 +13,15 @@ export default function MainContent() {
         <div className='section-main-contents'>
           <p className='main-contents-bigtxt'>FrontEnd Developer<br/>Web Publisher</p>
           <p className='main-contents-smalltxt'>웹 퍼블리셔로 시작하여, 현재는 프론트엔드 직무를 <br/>메인으로 일하고 있는 박성훈입니다.</p>
-          <a href='#achieved' className="main-sec-buttonbox">
+          <a href='#works' className="main-sec-buttonbox">
             <div className='anchor-behavior anchor-wBtn'>
               <p>프로젝트 바로가기</p>
-              <svg width="100%" height="100%" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <g clipPath="url(#clip0_410_326)">
-                <path d="M1.00042 6.99805C0.448134 6.99782 0.000231226 7.44534 1.04678e-07 7.99763C-0.000231017 8.54991 0.447297 8.99782 0.999582 8.99805L1.00042 6.99805ZM15.7068 8.71131C16.0975 8.32095 16.0978 7.68778 15.7074 7.29709L9.34611 0.930471C8.95574 0.539784 8.32258 0.539519 7.93189 0.929879C7.5412 1.32024 7.54094 1.95341 7.9313 2.34409L13.5858 8.00331L7.92657 13.6578C7.53588 14.0482 7.53561 14.6813 7.92597 15.072C8.31633 15.4627 8.9495 15.463 9.34019 15.0726L15.7068 8.71131ZM0.999582 8.99805L14.9996 9.00391L15.0004 7.00391L1.00042 6.99805L0.999582 8.99805Z" fill="currentcolor"></path>
+              <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+                <g id="SVGRepo_iconCarrier">
+                  <path d="M7 17L17 7M17 7H8M17 7V16" stroke="#000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
                 </g>
-                <defs>
-                <clipPath id="clip0_410_326">
-                <rect width="16" height="16" fill="#000"></rect>
-                </clipPath>
-                </defs>
               </svg>
             </div>
             <div className='anchor-behavior-under'></div>
@@ -57,8 +54,7 @@ export default function MainContent() {
               </h2>
             </div>
             <div className="experience-hbox hbox-right">
-              <p className='experience-hboxContentsTxt'>아래 프로젝트들은 정규직/프리랜서 재직 중 참여한 내용입니다. 초기 마크업 및 인터랙션 설계 및 구축을 담당하였으며 안드로이드, 아이폰 디바이스 반응형으로 제작했습니다.</p>
-              <p className='experience-hboxContentsTxt'>또한, 로딩 속도 개선, 반응성을 고려하여 JavaScript 최적화 경험이 있고 앱의 경우에는 TalkBack, ScreenReader 접근성 Aria를 이용하여 반영했습니다.</p>
+              <p className='experience-hboxContentsTxt'>With years of experience, we have had the opportunity to work on a wide range of projects for clients from all over the world, from building custom websites to developing complex web applications in Webflow. Explore our portfolio to see some of our most recent works and learn more about our experience and capabilities.</p>
             </div>
           </div>
           <Experience />
